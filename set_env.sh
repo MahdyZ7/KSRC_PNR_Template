dbb0fe6738e5273544029ce6a3f56472635e6a71
ayassin <ayassin@ksrc2.kunet.ae>
2025-06-16 14:23:50 +0400

    check point

---
+++ set_env.sh
@@ -0,0 +1,9 @@
+export ProjectName="GHzDivider"
+export TopModule="GHzDivider"
+
+export ProjectDir="$(pwd)"
+export ProjectRtlDir="${ProjectDir}/rtl"
+export ProjectTBDir="${ProjectDir}/testbench"
+export ProjectSimDir="${ProjectDir}/simulation"
+
+export TestBenchTopModule="${TopModule}_tb"
